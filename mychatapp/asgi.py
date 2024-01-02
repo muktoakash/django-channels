766e15f8025c2d8e61ad7384dd6c5fdbc4d8e34e
Mukto Akash <makash@conestogac.on.ca>
2024-01-02 17:42:55 -0500

    Correct imports

--- mychatapp/asgi.py
+++ mychatapp/asgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mychatapp.settings')
