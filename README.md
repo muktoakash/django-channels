ec9bee685576eed934dcd6c2d803210ea91fbfcd
Mukto Akash <makash@conestogac.on.ca>
2024-01-02 17:53:04 -0500

    Configure consumer

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # django-channels
 Implement a basic chatting app using django channels. Following [medium article](https://medium.com/atomic-loops/django-channels-is-all-you-need-94628dd6815c). 
 
-## Commands:
+## New Terms:
     `python -m pip install -U 'channels[daphne]'`
 Daphne is a HTTP and WebSockets protocol server for ASGI to power Django-Channels.
+
+Channels uses something called consumers. These consumers are like friendly assistants that know how to handle different types of real-time communication events, such as receiving a new message in the chat.
 
 ## Views:
 - (Chat Room lobby)[./chat/templates/chat/index.html]
--- mychatapp/asgi.py
+++ mychatapp/asgi.py
@@ -7,17 +7,42 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
+# import os
+
+# from channels.routing import ProtocolTypeRouter
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mychatapp.settings')
+
+# # application = get_asgi_application()
+
+# application = ProtocolTypeRouter( # new
+#     {
+#         "http": get_asgi_application(),
+#     }
+# )
+
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mychatapp.settings')
+from chat.routing import websocket_urlpatterns
 
-# application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter( # new
+import chat.routing
+
+application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
     }
 )
